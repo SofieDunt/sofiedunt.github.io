fa698436568acab655935aacb20b2ff294b10ee7
Sofie Duntugan <sofiedunt@gmail.com>
2021-08-28 18:45:19 -0700

    Theme

---
+++ src/components/navbar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navbar.css';
+
+function NavBar() {
+  return (
+      <div className={'nav-container'}>
+        <a href={'/'}>Sofie Duntugan</a>
+      </div>
+  );
+}
+
+export default NavBar;
