4c1312b2d7b6f676a371147021399b27fc6002aa
Sofie Duntugan <sofiedunt@gmail.com>
2021-12-17 23:04:33 -0800

    Mobile menu

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,22 +1,83 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './navbar.css';
-import {NAME, MenuSections, SectionNames} from "../../content";
+import { Icons, NAME, MenuSections, SectionNames } from "../../content";
+import { mobileWidth, getSectionFromHash } from '../../utils';
 
 function NavBar() {
-  const [active, setActive] = useState('');
+  const [active, setActive] = useState(getSectionFromHash(window.location.hash));
+  const [isMobile, setIsMobile] = useState(mobileWidth(window.innerWidth));
+  const [mobileActive, setMobileActive] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      const hasMobileWidth = mobileWidth(window.innerWidth);
+      if (!isMobile && hasMobileWidth) {
+        setIsMobile(true);
+      } else if (isMobile && !hasMobileWidth) {
+        setIsMobile(false);
+      }
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  })
+
+  const onClickMobileItem = (section) => {
+    setActive(section);
+    setMobileActive(false);
+  }
+
+  const MapLinks = (props) => {
+    const { className } = props;
+    const classNameState = (section) => {
+      return `${className} ${active === section ? 'active' : ''}`;
+    }
+    return (
+        <>
+          {MenuSections.map((section) => {
+            return (
+                <a href={`#${section}`} key={section} onClick={() => onClickMobileItem(section)}>
+                  <div className={classNameState(section)}>{SectionNames[section]}</div>
+                </a>
+            );
+          })}
+          </>
+    );
+  }
+
+  const DesktopMenu = () => {
+    return (
+        <div className={'links'}>
+          <MapLinks className={'item'} />
+        </div>
+    );
+  }
+
+  const MobileMenu = () => {
+    if (mobileActive) {
+      return (
+          <div className={'mobile-menu'}>
+            <div className={'links'} onClick={() => setMobileActive(false)}>
+              <img className={'item'} src={Icons.CLOSE} alt={'menu'}/>
+            </div>
+            <div className={'mobile-links'}>
+              <MapLinks className={'mobile-item'} />
+            </div>
+          </div>
+      );
+    }
+    else {
+      return (
+          <div className={'links'} onClick={() => setMobileActive(true)}>
+            <img className={'item'} src={Icons.MENU} alt={'menu'}/>
+          </div>);
+    }
+  }
 
   return (
       <div className={'nav-container'}>
         <a href={'/'} className={"main-link"}>{NAME}</a>
-        <div className={'links'}>
-          {MenuSections.map((section) => {
-            return (
-                <a href={`#${section}`} key={section} onClick={() => setActive(section)}>
-                  <div className={`item ${active === section ? 'active' : ''}`}>{SectionNames[section]}</div>
-                </a>
-            );
-          })}
-        </div>
+        {!isMobile ? <DesktopMenu /> : <MobileMenu /> }
       </div>
   );
 }
---
+++ src/utils.js
@@ -0,0 +1,21 @@
+/**
+ * Is the screen of the given width mobile?
+ * @param width the width of the screen
+ * @return {boolean}
+ */
+export const mobileWidth = (width) => {
+  return width < 600;
+}
+
+/**
+ * Returns hash location without the #.
+ * @param hash the hash location
+ * @return {string}
+ */
+export const getSectionFromHash = (hash) => {
+  if (hash.length > 0) {
+    return hash.substr(1);
+  } else {
+    return '';
+  }
+}
